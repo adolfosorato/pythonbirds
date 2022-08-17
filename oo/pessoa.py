0147f26768ed1a7566318b3597025dcc88444fe7
adolfosorato <adolfosorato@gmail.com>
2022-08-16 23:25:33 -0300

    criados métodos de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -8,6 +8,13 @@ class Pessoa:
 
     def cumprimentar(self):
         return f'Óla {id(self)}'
+    @staticmethod
+    def metodo_estatio():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos} '
 
 if __name__ == '__main__':
     Eduarda = Pessoa(nome='Eduarda')
@@ -28,3 +35,5 @@ if __name__ == '__main__':
     print(Lucianna.olhos)
     print(Eduarda.olhos)
     print(id(Pessoa.olhos), id(Lucianna.olhos), id (Eduarda.olhos))
+    print(Pessoa.metodo_estatio(), Lucianna.metodo_estatio())
+    print(Pessoa.nome_e_atributos_de_classe(), Lucianna.nome_e_atributos_de_classe())
